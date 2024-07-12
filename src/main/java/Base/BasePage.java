b411b7dc8fcae61cf334eb6e7634b5d120a25c9c
yohan <yohanvidusha@gmail.com>
2024-07-12 01:11:44 -0400

    New changes to test

--- src/main/java/Base/BasePage.java
+++ src/main/java/Base/BasePage.java
@@ -5,12 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Set;
 
 public class BasePage {
